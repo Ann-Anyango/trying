f40416075807e43d056e0611266379061ee4016a
Ann-Anyango <annmwangombe@gmail.com>
2023-04-12 21:34:07 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,39 @@
+fun main() {
+var book1=Library("borner a crime ")
+
+ println(book1)
+
+var book2= ( Library("A man in Green Dangary"))
+ println(book2)
+
+var books= listOf("adivance")
+ println(books)
+ var names= listOf("sermer")
+
+
+}
+//Project: Simple Library Management System
+//The goal of this project is to create a Simple Library Management System that
+//allows users to manage a collection of books by adding,
+//removing, and listing them. You will be working with Kotlin concepts
+//like variables, data types, arrays, array methods, strings,
+//string methods, functions, and classes.
+//
+
+data class Library(var books:String)
+ fun adding(name:MutableList<Library> ){
+  listOf("born")
+  println(name)
+
+
+ }
+fun removing(name:MutableList<Library>){
+
+
+}
+fun listing(name:MutableList<Library>){
+
+}
+
+
+
